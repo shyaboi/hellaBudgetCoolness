df9e0a9dab809451db9c12d6913304cfc9acf114
Ian <ian.shane.sears@gmail.com>
2020-04-10 09:22:29 -0700

    offline functioning

--- routes/api.js
+++ routes/api.js
@@ -1,34 +1,37 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
-router.post("/api/transaction", ({body}, res) => {
-  Transaction.create(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(404).json(err);
-    });
+// post requests for transactions
+router.post("/api/transaction", ({ body }, res) => {
+    Transaction.create(body)
+        .then(dbTransaction => {
+            res.json(dbTransaction);
+        })
+        .catch(err => {
+            res.status(404).json(err);
+        });
 });
 
-router.post("/api/transaction/bulk", ({body}, res) => {
-  Transaction.insertMany(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(404).json(err);
-    });
+router.post("/api/transaction/bulk", ({ body }, res) => {
+    Transaction.insertMany(body)
+        .then(dbTransaction => {
+            res.json(dbTransaction);
+        })
+        .catch(err => {
+            res.status(404).json(err);
+        });
 });
 
+// get request for alreay-posted transactions
 router.get("/api/transaction", (req, res) => {
-  Transaction.find({}).sort({date: -1})
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(404).json(err);
-    });
+    Transaction.find({})
+        .sort({ date: -1 })
+        .then(dbTransaction => {
+            res.json(dbTransaction);
+        })
+        .catch(err => {
+            res.status(404).json(err);
+        });
 });
 
 module.exports = router;
