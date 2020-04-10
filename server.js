7b2b959b4a8e5620f3874a1b0a275fe7a31727b7
Ian <ian.shane.sears@gmail.com>
2020-04-10 09:33:42 -0700

    heroku mongo db

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+// set up port and express
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+// connect all middleware
 app.use(logger("dev"));
 
 app.use(compression());
@@ -15,14 +17,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+// connect database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+    useNewUrlParser: true,
+    useFindAndModify: false
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+    console.log(`App running on port ${PORT}!`);
 });
